b907e8355b4d7d1da7612f55887ed62a11e4fa36
hyginn <boris.steipe@utoronto.ca>
2016-01-29 17:43:55 -0500

    First code

---
+++ R_Exercise-Subsetting.R
@@ -0,0 +1,18 @@
+# R_exercise-subsetting.R
+#
+# Practising filtering via R's three subsetting methods
+#
+# Boris Steipe Jan 2016
+
+
+# ===== Data ==========================================
+"Before we begin subsetting, we need some data to work with. I have provided a dataframe in the form of an RDA object within this project. Load it now:"
+
+data <- load("dataSet.RDA")
+head(data)
+
+# do this ...
+
+
+
+# [END]
